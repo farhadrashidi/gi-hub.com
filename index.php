0cfcd1fe5172d8e2a0b08898502a6424d85b2540
Soheil Rashidi <contact@soheilrashidi.com>
2024-10-13 18:40:25 +0330

    update

--- index.php
+++ index.php
@@ -58,8 +58,11 @@
 				<nav>
 				  <ul class="center-aligned">
 					<li><a href="#banner">Home</a></li>
+					<li><a href="#intro">Introduction</a></li>
+					<li><a href="#services">Services</a></li>
 					<li><a href="#companies">What We Hold</a></li>
 					<li><a href="#about">About Georgia</a></li>
+					<li><a href="#advantages">Advantages</a></li>
 					<li><a href="#investment">Investment</a></li>
 					<li><a href="#contact">Contact</a></li>
 				  </ul>
@@ -207,6 +210,60 @@
 					</div><!-- tp-banner end -->
 				</section><!-- banner end -->
 
+				<section id="intro">
+					<div class="content has-top-border site-bg">
+						<div class="frame small">
+							<div class="mini-section white-color has-top-margin">
+								<div class="relative-position">
+									<h4 class="white-color center-aligned">Georgian Investors Hub was established in 2018. The company was founded with the primary objective of becoming a leading investment holding company in the region, dedicated to nurturing business growth and economic development.</h4>
+									<br />
+									<h4 class="white-color center-aligned">Georgian Investors Hub operates as a strategic holding entity, focusing on the ownership and control of subsidiary companies across various economic sectors. While the company does not engage in the day-to-day operations of its subsidiaries, it plays a crucial role in defining their overall strategic direction, providing financial oversight, and managing resources to ensure sustained growth and stability.</h4>
+									<br />
+									<h4 class="white-color center-aligned">Since its inception, Georgian Investors Hub has expanded its portfolio by acquiring majority shares in key companies, enabling it to drive value creation and promote the growth of its subsidiaries. The company’s structured approach to investment and management has positioned it as a catalyst for innovation and business excellence within its sphere of influence.</h4>
+								</div><!-- relative-position end -->
+							</div><!-- mini-section end -->
+						</div><!-- frame small end -->
+					</div>
+
+					</div><!-- content end -->
+				</section><!-- intro end -->
+
+				<section id="services">
+					<div class="content has-top-border dark-bg">
+						<div class="frame medium">
+							<h2 class="white-color center-aligned">Services</h2>
+							<div class="mini-section white-color has-top-margin">
+								<div class="services">
+									<div class="service">
+										<h3 class="white-color">Investment Management</h3>
+										<p class="white-color">Acquisition and ownership of majority shares in subsidiaries to build a diversified portfolio.</p>
+									</div>
+
+									<div class="service">
+										<h3 class="white-color">Strategic Advisory</h3>
+										<p class="white-color">Defines and oversees the implementation of general policies and strategies for subsidiaries.</p>
+									</div>
+
+									<div class="service">
+										<h3 class="white-color">Financial Resource Management</h3>
+										<p class="white-color">Provision of financial resources and facilitation of financial transactions for subsidiaries.</p>
+									</div>
+
+									<div class="service">
+										<h3 class="white-color">Risk Management</h3>
+										<p class="white-color">Monitoring and managing the risks associated with the activities of subsidiary companies.</p>
+									</div>
+
+									<div class="service">
+										<h3 class="white-color">Business Development</h3>
+										<p class="white-color">Expanding the operations of subsidiaries in various economic fields as part of an overarching investment strategy.</p>
+									</div>
+								</div>
+							</div><!-- content end -->
+						</div><!-- content end -->
+					</div><!-- content end -->
+				</section><!-- about end -->
+
 				<section id="companies">
 					<div class="content has-top-border">
 						<div class="frame">
@@ -264,11 +321,11 @@
 							<h2 class="white-color center-aligned">About Georgia</h2>
 							<div class="mini-section white-color has-top-margin">
 								<div class="relative-position">
-									<blockquote class="firstQuote active"><span>G</span>eorgia a little piece of living history sits on the crossroads of Europe and Asia. Georgia is a rapidly modernizing country with ancient traditions of hospitality that welcomes you!!!
+									<blockquote class="firstQuote active">Georgia a little piece of living history sits on the crossroads of Europe and Asia. Georgia is a rapidly modernizing country with ancient traditions of hospitality that welcomes you!!!
 									Few nations in the world offer such diversity as Georgia. Within its small boundaries, Georgia has the highest mountains in Europe and a total of 12 peaks higher than Mont Blank. From glaciers to beaches, alpine meadows to semi-desert and nearly 40 percent of her land covered with forest; Georgia truly has a diverse landscape.
 									Everywhere you look in Georgia you see the mountain top churches , castles, and ancient wine cellars. Georgia is the cradle of wine culture: here eight thousand years ago, mankind discovered the art of wine making, and today, is the home of over 500 grape varieties. Georgians still use the ancient traditions of wine making- the Kvevri; a huge clay vessel buried in the ground and many Georgian wines have won international awards and recognition. </blockquote>
 
-									<blockquote class="secondQuote"><span>G</span>eorgia is famous for its hospitality and the guest here is considered as the gift from God and you better believe Georgia has lots to offer a guest. Among these offerings is Supra- the table feast, with delicious dishes, polyphonic singing and of course the rituals of toasting, that makes the Georgian experience unforgettable.
+									<blockquote class="secondQuote">Georgia is famous for its hospitality and the guest here is considered as the gift from God and you better believe Georgia has lots to offer a guest. Among these offerings is Supra- the table feast, with delicious dishes, polyphonic singing and of course the rituals of toasting, that makes the Georgian experience unforgettable.
 									The heart of Georgia is Tbilisi, set among the hills and mountains. Full of ancient churches, a majestic castle and traditional houses, Tbilisi is one of the most picturesque and charming cities of the country.
 									Being the main connection to East and West, Georgia was part of the historical Silk Route. Even today the country maintains a very important trading zone.
 									Georgia is an all year round destination. There are ski resorts in Georgia, the most famous being Gudauri and Bakuriani.  These resorts, located at high elevations enjoy snows until the middle of spring. The beaches on the Black Sea are welcoming guests in summer months.
@@ -282,6 +339,89 @@
 						</div><!-- frame small end -->
 					</div><!-- content end -->
 				</section><!-- about end -->
+
+				<section id="advantages">
+					<div class="content has-top-border">
+						<div class="frame large">
+							<h2 class="center-aligned">Advantages of Investing in Georgia</h2>
+							<p class="intro center-aligned">Georgia is uniquely positioned at the crossroads of Europe and Asia, sharing land borders with Azerbaijan, Armenia, Turkey, and Russia. Its strategic location along the Black Sea provides access to significant trade routes, connecting through the Kerch Strait in the north to the Sea of Azov and, in the southwest, via the Bosphorus Strait to the Sea of Marmara and the Mediterranean Sea. This access extends further through the Mediterranean to the Atlantic Ocean, effectively linking Georgia with Russia, Romania, Bulgaria, Ukraine, and Turkey, enhancing its importance as a regional trade hub.</p>
+							<div class="mini-section  has-top-margin">
+								<div class="advantages">
+									<div class="advantage">
+										<h5>Business-Friendly Legislation</h5>
+										<p>Georgia offers a favorable business environment, supported by laws that protect foreign investors.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>100% Foreign Ownership Rights</h5>
+										<p>Foreign investors can fully own businesses and assets, ensuring complete control and flexibility.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Inheritance Rights</h5>
+										<p>The Constitution of Georgia recognizes and protects the right of inheritance for all investors.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>WTO Membership</h5>
+										<p>As a member of the World Trade Organization (WTO) since 2000, Georgia aligns with international trade standards, enhancing transparency and trust.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Generalized System of Preferences (GSP)</h5>
+										<p>Georgia has established the GSP with the US, Canada, Switzerland, Japan, and Norway, enabling Georgian goods to enter these markets at reduced tariffs.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>GSP with EU Countries</h5>
+										<p>Georgia benefits from a GSP agreement with the European Union, allowing the export of over 7,200 products with customs exemptions.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Growing Exports to China</h5>
+										<p>In 2021, Georgia's exports to China saw significant growth, making China its largest export market, as reported by the Georgian National Bureau of Statistics.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Tax and Customs Benefits</h5>
+										<p>Exports and re-exports are exempt from VAT and customs tariffs, ensuring a cost-effective trade environment.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>New Customs System</h5>
+										<p>The country has modernized its customs procedures, further simplifying the movement of goods across borders.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Free Trade Agreements (FTA)</h5>
+										<p>Georgia has established FTAs with countries such as Belarus, Ukraine, Azerbaijan, Armenia, Russia, Moldova, Kazakhstan, Kyrgyzstan, Tajikistan, Uzbekistan, Turkmenistan, and Turkey, facilitating seamless trade within the region.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Deep and Comprehensive Free Trade Area (DCFTA)</h5>
+										<p>A DCFTA agreement with EU countries, signed on June 27, 2014, enhances Georgia’s integration into European markets.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Skilled Workforce</h5>
+										<p>Over 30% of the workforce holds a university degree, and the average monthly salary is below the European average, providing access to a highly educated yet cost-effective labor pool. Additionally, 50% of the unemployed population is between the ages of 20 and 34, representing a large pool of potential talent.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Open Trade Policies</h5>
+										<p>Georgia has no trade restrictions with countries around the world and is committed to a free trade policy, supporting sustained economic growth and investment stability.</p>
+									</div>
+
+									<div class="advantage">
+										<h5>Growing Industrial Sector</h5>
+										<p>The industrial sector plays a crucial role in the Georgian economy. In recent years, there has been substantial growth, with efforts to establish new factories and production facilities. Key industries include food and beverage packaging, which form a significant portion of Georgia’s industrial output.</p>
+									</div>
+								</div>
+							</div><!-- content end -->
+						</div><!-- content end -->
+					</div><!-- content end -->
+				</section><!-- about end -->
+
 				<section id="investment">
 					<div class="content has-top-border dark-bg">
 						<div class="frame small">
